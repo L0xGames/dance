a648243239e48f6ca1abf24771c8620a2f23cc72
L0xGames <kiandousti@yahoo.de>
2019-09-11 01:27:22 +0200

    first commit

---
+++ app/src/main/java/com/example/danceexplosion/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.danceexplosion;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    private ConstraintLayout constraintLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //init
+        constraintLayout=findViewById(R.id.constri);
+        webView=findViewById(R.id.webview);
+        WebSettings webSettings=webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new CustomWebViewClient());
+        webView.loadUrl("https://www.dance-explosion.de");
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (webView.canGoBack()) {
+                        webView.goBack();
+                    } else {
+                        //do nothing
+                    }
+                    return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+    // This allows for a splash screen
+// (and hide elements once the page loads)
+    private class CustomWebViewClient extends WebViewClient {
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            constraintLayout.setVisibility(View.GONE);
+            webView.setVisibility(View.VISIBLE);
+
+        }
+    }
+}
